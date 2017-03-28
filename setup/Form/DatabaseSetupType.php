b45fb07e16200bb80ec137a67dd521c2e8c05c56
Robert Down <robertdown@live.com>
2017-03-27 23:42:24 -0400

    Render the form via the twig and form object

--- setup/Form/DatabaseSetupType.php
+++ setup/Form/DatabaseSetupType.php
@@ -46,20 +46,48 @@ class DatabaseSetupType extends AbstractType
         $builder
             ->add('host', TextType::class, array('data' => 'localhost'))
             ->add('port', IntegerType::class, array('data' => '3306'))
-            ->add('dbname', TextType::class, array('data' => 'openemr'))
+            ->add('dbname', TextType::class, array('data' => 'openemr', 'label' => 'DB Name'))
             ->add('login', TextType::class, array('data' => 'openemr'))
             ->add('pass', PasswordType::class, array('data' => 'openemr'))
-            ->add('rootuser', TextType::class, array('data' => 'openemr'))
-            ->add('rootpass', PasswordType::class)
-            ->add('loginhost', TextType::class, array('data' => 'localhost'))
-            ->add('collate', ChoiceType::class)
-            ->add('user', TextType::class, array('data' => 'user'))
-            ->add('iuserpass', PasswordType::class)
-            ->add('iufname', TextType::class)
-            ->add('iuname', TextType::class)
-            ->add('igroup', TextType::class)
+            ->add('rootuser', TextType::class, array('data' => 'openemr', 'label' => 'DB Root User'))
+            ->add('rootpass', PasswordType::class, array('label' => 'DB Root Pass'))
+            ->add('loginhost', TextType::class, array('data' => 'localhost', 'label' => 'Login Host'))
+            ->add('collate', ChoiceType::class, array(
+                'choices' => array(
+                    'bin' => 'utf8_bin',
+                    'czech' => 'utf8_czech_ci',
+                    'danish' => 'utf8_danish_ci',
+                    'esperanto' => 'utf8_esperanto_ci',
+                    'estonian' => 'utf8_estonian_ci',
+                    'general' => 'utf8_general_ci',
+                    'icelandic' => 'utf8_icelandic_ci',
+                    'latvian' => 'utf8_latvian_ci',
+                    'lithuanian' => 'utf8_lithuanian_ci',
+                    'persian' => 'utf8_persian_ci',
+                    'polish' => 'utf8_polish_ci',
+                    'roman' => 'utf8_roman_ci',
+                    'romanian' => 'utf8_romanian_ci',
+                    'slovak' => 'utf8_slovak_ci',
+                    'slovenian' => 'utf8_slovenian_ci',
+                    'spanish2 (traditional)' => 'utf8_spanish2_ci',
+                    'spanish (modern)' => 'utf8_spanish_ci',
+                    'swedish' => 'utf8_swedish_ci',
+                    'turkish' => 'utf8_turkish_ci',
+                    'unicode (german, french, russian, armeniam, greek)' => 'utf8_unicode_ci',
+                    'none (do not force uft-8' => ''
+                ),
+                'data' => 'general',
+                'label' => 'UTF-8 Collation')
+            )
+            ->add('user', TextType::class, array('data' => 'user', 'label' => 'Initial User'))
+            ->add('iuserpass', PasswordType::class, array('label' => 'Initial User Password'))
+            ->add('iufname', TextType::class, array('label' => 'Initial User First Name'))
+            ->add('iuname', TextType::class, array('label' => 'Initial User Last Name'))
+            ->add('igroup', TextType::class, array('label' => 'Initial Group'))
             ->add('source', ChoiceType::class)
-            ->add('clone_database', CheckboxType::class, array('required' => false))
+            ->add('clone_database', CheckboxType::class, array(
+                'required' => false,
+                'label' => 'Clone the source site\'s database instead of creating a fresh one '))
             ->add('submit', SubmitType::class, array(
                 'label' => 'Continue',
                 'attr' => array(
